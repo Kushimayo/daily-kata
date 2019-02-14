e39debf0fbe483a54dafdc777c534373c8057bc1
Kushimayo <akstp31@naver.com>
2019-02-14 09:58:18 +0900

    day_5 work commit

---
+++ WordWrap/day_5/Kay/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WordWrapper의_인스턴스를_생성할수있다()
+        {
+            var wordWrapper = new WordWrapper();
+        }
+
+        [TestMethod]
+        public void Coloumn크기가_word보다클때에는_doWrap시_word가_반환된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("test", wordWrapper.doWrap("test", 7));
+        }
+
+        [TestMethod]
+        public void 두개의단어중_두번째단어보다_coloumn크기가작다면_두번째단어는_Wrap되어야한다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("hello--world", wordWrapper.doWrap("hello world", 7));
+        }
+
+        [TestMethod]
+        public void 한줄에많은단어가있는상태로_Wrap_되어야한다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("a lot of--words for--a single--line", wordWrapper.doWrap("a lot of words for a single line", 10));
+        }
+
+        [TestMethod]
+        public void 이건_테스트_문제_()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("this--is a--test", wordWrapper.doWrap("this is a test", 4));
+        }
+
+    }
+}
---
+++ WordWrap/day_5/Kay/WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public WordWrapper()
+        {
+        }
+
+        public string doWrap(string word, int col_size)
+        {
+            string result_string = "";
+            char[] split_key_word = { ' ' };
+            if (word.Length > col_size)
+            {
+                int last_space = word.Substring(0, col_size).LastIndexOf(" ");
+                if (last_space != -1)
+                {
+                    result_string += word.Substring(0, last_space);
+                    result_string += "--";
+                    result_string += doWrap(word.Substring(last_space + 1), col_size);
+                } else
+                {
+
+                }
+                
+            } else
+            {
+                result_string = word;
+            }
+
+            return result_string;
+        }
+    }
+}
