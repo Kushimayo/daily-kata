df829165987a985567736e60a6b7fee7e1e3b1a2
Kushimayo <akstp31@naver.com>
2019-02-15 09:43:30 +0900

    day_6 work commit

---
+++ WordWrap/day_6/Kay/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WordWrapper클래스_인스턴스를_생성할수있다()
+        {
+            var wordWrapper = new WordWrapper();
+        }
+
+        [TestMethod]
+        public void 가로행크기가_충분하다면_Word_Wrap하지_않는다()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("test", wordWrapper.doWrap("test", 7));
+        }
+
+        [TestMethod]
+        public void Wrap이필요한경우_스페이스_포함_뒤에_단어는_coloumn_size보다작다면_스페이스를_기준으로_Wrap한다()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("hello--world", wordWrapper.doWrap("hello world", 7));
+        }
+
+        [TestMethod]
+        public void 한줄에_coloumn_size보다작은단어가_많이_포함되어있어도_Wrap되어야한다()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("a lot of--words for--a single--line", wordWrapper.doWrap("a lot of words for a single line", 10));
+        }
+
+        [TestMethod]
+        public void 한단어의크기가_coloumn_size와같다면_해당단어끝에서_Wrap_되어야한다()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("this--is a--test", wordWrapper.doWrap("this is a test", 4));
+        }
+
+        [TestMethod]
+        public void 한단어의크기가_coloumn_size_보다클때_앞에스페이스바가있더라도_단어중간을_Wrap한다()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("a long--word", wordWrapper.doWrap("a longword", 6));
+        }
+    }
+}
---
+++ WordWrap/day_6/Kay/WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public WordWrapper()
+        {
+        }
+
+        public string doWrap(string word, int col_size)
+        {
+            string result_string = "";
+            int last_wrap_index = 0;
+            int preview_index = 0;
+            bool need_space = true;
+
+            if (word.Length > col_size)
+            {
+                char[] split_key_word = { ' ' };
+                string[] split_strings = word.Split(split_key_word);
+                if (split_strings[0].Length > col_size)
+                {
+
+                } else
+                {
+                    result_string += split_strings[0];
+                }
+
+                for (int i = 1; i < split_strings.Length; i++)
+                {
+                    preview_index = i;
+                    if ((result_string.Length - last_wrap_index) + split_strings[i].Length + 1 > col_size)
+                    {
+                        if (split_strings[i].Length > col_size)
+                        {
+                            int one_word_wrap_index = (col_size - (result_string.Length - last_wrap_index) - 1);
+                            if (need_space)
+                                result_string += " ";
+                            result_string += split_strings[i].Substring(0, one_word_wrap_index);
+                            result_string += "--";
+                            last_wrap_index = result_string.Length;
+                            split_strings[i] = split_strings[i].Substring(one_word_wrap_index);
+                            i -= 1;
+                        } else
+                        {
+                            result_string += "--";
+                            last_wrap_index = result_string.Length;
+                            result_string += split_strings[i];
+                        }
+                    } else
+                    {
+                        if (need_space)
+                            result_string += " ";
+                        result_string += split_strings[i];
+                    }
+                    if (preview_index != i)
+                        need_space = false;
+                    else
+                        need_space = true;
+                }
+            } else
+            {
+                result_string += word;
+            }
+
+            return result_string;
+        }
+    }
+}
