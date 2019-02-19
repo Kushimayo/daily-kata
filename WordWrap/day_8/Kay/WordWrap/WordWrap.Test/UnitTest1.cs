457e75dee7e31ccac50dc1ff99905c1f5ab08ab4
Kushimayo <akstp31@naver.com>
2019-02-19 09:30:46 +0900

    day_8 work commit

---
+++ WordWrap/day_8/Kay/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WordWrapper인스턴스를_생성할수있다()
+        {
+            var wordWrapper = new WordWrapper();
+        }
+
+        [TestMethod]
+        public void Coloumn_크기가_충분하다면_Wrap없이_string이리턴된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("test", wordWrapper.doWrap("test", 7));
+        }
+
+        [TestMethod]
+        public void 두개의_단어_중_두번째에_Coloumn이걸쳐있고_두번째단어가_Coloumn_크기보다작다면_space로_Wrap된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("hello--world", wordWrapper.doWrap("hello world", 7));
+        }
+
+        [TestMethod]
+        public void 여러단어의_긴단어라도_space를_기점으로_Wrap된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("a lot of--words for--a single--line", wordWrapper.doWrap("a lot of words for a single line", 10));
+        }
+
+        [TestMethod]
+        public void 한단어의크기가_Coloumn크기가같더라도_Wrap된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("this--is a--test", wordWrapper.doWrap("this is a test", 4));
+        }
+
+        [TestMethod]
+        public void 두개의_단어중_두번째에_Coloumn이걸쳐있고_두번째단어가_Coloumn_크기보다_크다면_두번째단어에서Wrap_된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("a long--word", wordWrapper.doWrap("a longword", 6));
+        }
+
+        [TestMethod]
+        public void 하나의단어가_Coloumn크기보다크다면_단어가Wrap된다()
+        {
+            var wordWrapper = new WordWrapper();
+            Assert.AreEqual("areall--ylongw--ord", wordWrapper.doWrap("areallylongword", 6));
+        }
+    }
+}
---
+++ WordWrap/day_8/Kay/WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public string doWrap(string word, int col_size)
+        {
+            string result_string = "";
+
+            if (word.Length > col_size)
+            {
+                char[] split_key_word = { ' ' };
+                string[] split_strings = word.Split(split_key_word);
+                int last_wrap_index = 0;
+                int need_space = 0;
+                int prev_index = 0;
+                int available_word_count = 0;
+
+                for (int i = 0; i < split_strings.Length; i++)
+                {
+                    prev_index = i;
+                    available_word_count = result_string.Length - last_wrap_index;
+                    if (available_word_count + split_strings[i].Length + need_space > col_size)
+                    {
+                        if (split_strings[i].Length > col_size)
+                        {
+                            int long_word_wrap_index = col_size - available_word_count - need_space;
+                            result_string = add_space(result_string, need_space);
+                            result_string += split_strings[i].Substring(0, long_word_wrap_index);
+                            result_string += "--";
+                            last_wrap_index = result_string.Length;
+                            split_strings[i] = split_strings[i].Substring(long_word_wrap_index);
+                            i -= 1;
+                        } else
+                        {
+                            result_string += "--";
+                            last_wrap_index = result_string.Length;
+                            result_string += split_strings[i];
+                        }
+                    } else
+                    {
+                        result_string = add_space(result_string, need_space);
+                        result_string += split_strings[i];
+                    }
+
+                    if (prev_index != i)
+                        need_space = 0;
+                    else
+                        need_space = 1;
+                }
+            } else
+            {
+                result_string = word;
+            }
+
+            return result_string;
+        }
+        public string add_space(string word, int need_space)
+        {
+            if (need_space != 0)
+                word += " ";
+            return word;
+        }
+    }    
+}
