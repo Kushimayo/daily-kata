8f2649defef27a5b50b3adf79600d3874bdd6c56
Kushimayo <akstp31@naver.com>
2019-02-21 09:32:07 +0900

    day_10 work commit

---
+++ WordWrap/day_10/Kay/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void 인스턴스를_생성할수있다()
+        {
+            var wordWrapper = new WordWrapper();
+        }
+
+        [TestMethod]
+        public void col_크기보다작은한단어()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("test", wordWrapper.Wrap("test", 7));
+        }
+
+        [TestMethod]
+        public void col_크기보다작은두단어()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("hello--world", wordWrapper.Wrap("hello world", 7));
+        }
+
+        [TestMethod]
+        public void col_크기보다작은여러단어()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("a lot of--words for--a single--line", wordWrapper.Wrap("a lot of words for a single line", 10));
+        }
+
+        [TestMethod]
+        public void col_크기와_같은단어포함()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("this--is a--test", wordWrapper.Wrap("this is a test", 4));
+        }
+
+        [TestMethod]
+        public void col_크기보다큰단어가_스페이스뒤에_포함된_두단어()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("a long--word", wordWrapper.Wrap("a longword", 6));
+        }
+
+        [TestMethod]
+        public void col_크기보다큰한단어()
+        {
+            var wordWrapper = new WordWrapper();
+
+            Assert.AreEqual("areall--ylongw--ord", wordWrapper.Wrap("areallylongword", 6));
+        }
+    }
+}
---
+++ WordWrap/day_10/Kay/WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public WordWrapper()
+        {
+        }
+
+        public string Wrap(string word, int col_size)
+        {
+            string result = "";
+
+            if (word.Length > col_size)
+            {
+                int prev_space_index = word.Substring(0, col_size).LastIndexOf(' ');
+                int post_space_index = word.Substring(col_size).IndexOf(' ');
+
+                if (post_space_index == 0)
+                    result += doWrap(word, col_size, col_size, 1);
+                else if (prev_space_index == -1)
+                    result += doWrap(word, col_size, col_size, 0);
+                else
+                {
+                    int one_word_size = 0;
+                    if (post_space_index == -1)
+                        one_word_size = word.Length - prev_space_index;
+                    else
+                        one_word_size = col_size - prev_space_index + post_space_index;
+
+                    if (one_word_size > col_size)
+                        result += doWrap(word, col_size, col_size, 0);
+                    else
+                        result += doWrap(word, prev_space_index, col_size, 1);
+                }
+            } else
+                result = word;
+
+            return result;
+        }
+
+        public string doWrap(string word, int wrap_index, int col_size, int space_offset)
+        {
+            string result = word.Substring(0, wrap_index);
+            result += "--";
+            result += Wrap(word.Substring(wrap_index + space_offset), col_size);
+            return result;
+        }
+    }
+}
