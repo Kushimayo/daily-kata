10439c21d3e8e465b23f83e2f63ec32517f4095e
Kushimayo <akstp31@naver.com>
2019-03-12 10:25:09 +0900

    bowling not complete day_1

---
+++ BowlingKata/day_1/BowlingGame/BowlingGame.Test/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Game game;
+        [TestMethod]
+        public void 인스턴스를생성할수있다()
+        {
+            game = new Game();
+        }
+
+        private void rollMany(int count, int pins)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                game.roll(pins);
+            }
+        }
+
+        [TestMethod]
+        public void 한핀도못맞추면0점()
+        {
+            game = new Game();
+            rollMany(20, 0);
+            Assert.AreEqual(0, game.score());
+        }
+
+        [TestMethod]
+        public void 계속1핀만맞추면20점()
+        {
+            game = new Game();
+            rollMany(20, 1);
+            Assert.AreEqual(20, game.score());
+        }
+
+        [TestMethod]
+        public void 스페어처리하면_다음프레임으로보너스()
+        {
+            game = new Game();
+            game.roll(9);
+            game.roll(1);
+            game.roll(5);
+            rollMany(17, 0);
+            Assert.AreEqual(20, game.score());
+        }
+    }
+}
---
+++ BowlingKata/day_1/BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int gameScore;
+        private int[] rolls;
+        private int currentRoll;
+        public Game()
+        {
+            gameScore = 0;
+            rolls = new int[21];
+            currentRoll = 0;
+        }
+
+        public void roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+            gameScore += pins;
+        }
+
+        public int score()
+        {
+            return gameScore;
+        }
+    }
+}
